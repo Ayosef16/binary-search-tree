3e81789b2ca7e450aa047817e4d0332b0a30e33e
Ayosef <syrivera@uc.cl>
2023-07-17 11:18:58 -0400

    Add tree factory and insert method

--- tree.js
+++ tree.js
@@ -1,9 +1,90 @@
 // Create a node factory
 
 const nodeFactory = () => {
-  let data = 10;
-  let leftChild = null;
-  let rightChild = null;
+  let data = null;
+  let left = null;
+  let right = null;
 
-  return { data, leftChild, rightChild };
+  return { data, left, right };
 };
+
+const treeFactory = (array) => {
+  const root = buildTree(array);
+
+  const insert = (value) => {
+    let currentNode = root;
+    let parentNode;
+    while (currentNode) {
+      parentNode = currentNode;
+      if (value < currentNode.data) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.data) {
+        currentNode = currentNode.right;
+      } else {
+        return;
+      }
+    }
+    // Create a new node
+    let node = nodeFactory();
+    node.data = value;
+    // Insert the node
+    if (node.data < parentNode.data) {
+      parentNode.left = node;
+    } else {
+      parentNode.right = node;
+    }
+  };
+
+  return { root, insert };
+};
+
+const buildTree = (array) => {
+  // Remove duplicates from the array
+  const newArray = [...new Set(array)];
+  // Sort elements from the array, ascending order
+  const sortedArray = newArray.sort((a, b) => a - b);
+
+  // Get the array length and check if it's empty
+  let length = sortedArray.length;
+  if (length === 0) return null;
+
+  // Get the middle element of the array
+  let middle = Math.floor((length - 1) / 2);
+
+  // Create the root node and assign it a value
+  const root = nodeFactory();
+  root.data = sortedArray[middle];
+
+  // Create left and right arrays
+  let leftTree = sortedArray.slice(0, middle);
+  let rightTree = sortedArray.slice(middle + 1, length);
+
+  // Builds the left and right tree into the root
+  root.left = buildTree(leftTree);
+  root.right = buildTree(rightTree);
+
+  return root;
+};
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const testArray = [99, 11, 45, 29, 73, 1, 1, 99, 69, 1, 36, 50, 81];
+
+testTree = treeFactory(testArray);
+testTree.insert(2);
+testTree.insert(69);
+testTree.insert(77);
+testTree.insert(66);
+testTree.insert(66);
+prettyPrint(testTree.root);
