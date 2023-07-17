9b822069a7d0eb3b6b8010e80084ed67493845d9
Ayosef <syrivera@uc.cl>
2023-07-17 18:57:29 -0400

    Add delete node feature

--- tree.js
+++ tree.js
@@ -34,8 +34,60 @@ const treeFactory = (array) => {
     return rootNode;
   };
 
-  const deleteNode = (value) => {
-    root = deleteRec(root, value);
+  // Delete the node from the tree
+  const deleteNode = (value, rootNode = root) => {
+    // Base case
+    if (rootNode === null) return null;
+
+    // Travel through the tree recursively
+    if (value < rootNode.data) {
+      rootNode.left = deleteNode(value, rootNode.left);
+      return rootNode;
+    } else if (value > rootNode.data) {
+      rootNode.right = deleteNode(value, rootNode.right);
+      return rootNode;
+    }
+
+    // When the rootNode data is equual to the value
+    // If both childs are null, make the node null
+    if (rootNode.left === null && rootNode.right === null) {
+      return null;
+    }
+    // If left node is null and right is not, make the node be the right child
+    else if (rootNode.left === null) {
+      return rootNode.right;
+    }
+    // If right node is null and left is not, make the node be the left child
+    else if (rootNode.right === null) {
+      return rootNode.left;
+    }
+    // If both childs do exist
+    else {
+      // Define succesor that will replace current node and his parent (always look into the right child of the node)
+      let succesorParent = rootNode;
+      let succesor = rootNode.right;
+
+      // Look for the min value on the succesor
+      while (succesor.left) {
+        succesorParent = succesor;
+        succesor = succesor.left;
+      }
+
+      // Change node data to the succesor data
+      rootNode.data = succesor.data;
+
+      // Change parent succesor left child to the succesor right child if it's not the root
+      if (succesorParent !== rootNode) {
+        succesorParent.left = succesor.right;
+      }
+      // If it's the root
+      else {
+        succesorParent.right = succesor.right;
+      }
+
+      // Return new node with the changes
+      return rootNode;
+    }
   };
 
   // Return the node that contains the value, otherwise return null
@@ -97,7 +149,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-const testArray = [99, 11, 45, 29, 73, 1, 1, 99, 69, 1, 36, 50, 81];
+const testArray = [99, 11, 45, 29, 73, 1, 1, 99, 69, 1, 36, 50, 81, 100];
 
 testTree = treeFactory(testArray);
 testTree.insertNode(2);
@@ -105,6 +157,11 @@ testTree.insertNode(69);
 testTree.insertNode(77);
 testTree.insertNode(66);
 testTree.insertNode(66);
-// prettyPrint(testTree.root);
-prettyPrint(testTree.findNode(45));
-prettyPrint(testTree.findNode(11));
+prettyPrint(testTree.root);
+// testTree.deleteNode(1);
+// testTree.deleteNode(29);
+// testTree.deleteNode(50);
+testTree.deleteNode(81);
+prettyPrint(testTree.root);
+// prettyPrint(testTree.findNode(45));
+// prettyPrint(testTree.findNode(11));
