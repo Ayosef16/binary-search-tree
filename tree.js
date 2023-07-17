c58849993b17b540a8bbf9f2a43f46263842cb3a
Ayosef <syrivera@uc.cl>
2023-07-17 11:58:35 -0400

    Refactor insert method with recursion

--- tree.js
+++ tree.js
@@ -9,33 +9,53 @@ const nodeFactory = () => {
 };
 
 const treeFactory = (array) => {
-  const root = buildTree(array);
+  let root = buildTree(array);
 
-  const insert = (value) => {
-    let currentNode = root;
-    let parentNode;
-    while (currentNode) {
-      parentNode = currentNode;
-      if (value < currentNode.data) {
-        currentNode = currentNode.left;
-      } else if (value > currentNode.data) {
-        currentNode = currentNode.right;
-      } else {
-        return;
-      }
-    }
-    // Create a new node
-    let node = nodeFactory();
-    node.data = value;
-    // Insert the node
-    if (node.data < parentNode.data) {
-      parentNode.left = node;
-    } else {
-      parentNode.right = node;
-    }
+  // const insert = (value) => {
+  //   let currentNode = root;
+  //   let parentNode;
+  //   while (currentNode) {
+  //     parentNode = currentNode;
+  //     if (value < currentNode.data) {
+  //       currentNode = currentNode.left;
+  //     } else if (value > currentNode.data) {
+  //       currentNode = currentNode.right;
+  //     } else {
+  //       return;
+  //     }
+  //   }
+  //   // Create a new node
+  //   let node = nodeFactory();
+  //   node.data = value;
+  //   // Insert the node
+  //   if (node.data < parentNode.data) {
+  //     parentNode.left = node;
+  //   } else {
+  //     parentNode.right = node;
+  //   }
+  // };
+  const insertNode = (value) => {
+    root = insertRec(root, value);
   };
 
-  return { root, insert };
+  const insertRec = (rootNode, value) => {
+    let node = nodeFactory();
+    node.data = value;
+    if (rootNode === null) {
+      rootNode = node;
+      return rootNode;
+    }
+    if (value < rootNode.data) {
+      rootNode.left = insertRec(rootNode.left, value);
+    } else if (value > rootNode.data) {
+      rootNode.right = insertRec(rootNode.right, value);
+    }
+    return rootNode;
+  };
+
+  const deleteNode = (value) => {};
+
+  return { root, insertNode, deleteNode };
 };
 
 const buildTree = (array) => {
@@ -82,9 +102,9 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 const testArray = [99, 11, 45, 29, 73, 1, 1, 99, 69, 1, 36, 50, 81];
 
 testTree = treeFactory(testArray);
-testTree.insert(2);
-testTree.insert(69);
-testTree.insert(77);
-testTree.insert(66);
-testTree.insert(66);
+testTree.insertNode(2);
+testTree.insertNode(69);
+testTree.insertNode(77);
+testTree.insertNode(66);
+testTree.insertNode(66);
 prettyPrint(testTree.root);
